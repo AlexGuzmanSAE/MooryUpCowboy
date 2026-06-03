8c13c3ae69b9c247d81f6d06bbc19c958260ee00
JaimeAyoub <>
2026-06-03 09:08:50 -0600

    IA de vaca casi terminada

--- Assets/Scripts/COW_IA.cs
+++ Assets/Scripts/COW_IA.cs
@@ -1,17 +1,19 @@
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using System.Collections;
 
 
 public class COW_IA : MonoBehaviour
 {
     public GameObject prefabForPoints;
 
-    public List<Transform> collisionPoints = new List<Transform>();
-    
-    
+    public List<GameObject> collisionPoints = new List<GameObject>();
+
+
 
     public float radius;
-    
+
 
     public enum cowStates
     {
@@ -32,52 +34,36 @@ public class COW_IA : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             ChoosePoint();
         }
-    }
-
-    public void GenerateCircle()
-    {
-
-        collisionPoints.Clear();
-        Collider[] hitColliders =  Physics.OverlapSphere(transform.position, radius);
-
-
-
-        foreach (Collider collider in hitColliders)
+        if (currentCowState == cowStates.Idle)
         {
-            float distance = Vector3.Distance(transform.position, collider.transform.position);
-            Debug.Log(distance);
-                collisionPoints.Add(collider.transform);
-                Debug.Log(collider.transform.name);     
-
+            StartCoroutine(waitToChoosePoint());
         }
     }
+
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, radius);
         if (collisionPoints.Count > 0)
         {
-            foreach (Transform pos in collisionPoints)
+            foreach (GameObject pos in collisionPoints)
             {
                 Gizmos.DrawSphere(pos.transform.position, 1.1f);
             }
         }
 
-       
-    }
-
-    private void ChangeStateCow()
-    {
 
     }
 
     private void initialitePoints()
     {
-        Vector3 currentPos = transform.position;    
+
+        Vector3 currentPos = transform.position;
         for (int i = 0; i < 8; i++)
         {
             float angle = i * 45;
@@ -88,26 +74,70 @@ public class COW_IA : MonoBehaviour
                    Mathf.Cos(angle * Mathf.Deg2Rad) * radius
                );
             Vector3 spawnPos = currentPos + spawnOffset;
-            GameObject newPoint = Instantiate(prefabForPoints,spawnPos,rot);
-            collisionPoints.Add(newPoint.transform);
+            GameObject newPoint = Instantiate(prefabForPoints, spawnPos, rot);
+            collisionPoints.Add(newPoint);
         }
+       // StartCoroutine(waitToChoosePoint());
 
     }
+
+    public void MovePointsCow()
+    {
+        if (collisionPoints.Count > 0)
+        {
+            Vector3 currentPos = transform.position;
+            float i = 0;
+            foreach (GameObject pos in collisionPoints)
+            {
+                float angle = i * 45;
+                Quaternion rot = Quaternion.Euler(0, angle, 0);
+                Vector3 spawnOffset = new Vector3(
+                       Mathf.Sin(angle * Mathf.Deg2Rad) * radius,
+                       0,
+                       Mathf.Cos(angle * Mathf.Deg2Rad) * radius
+                   );
+                i++;
+                Vector3 spawnPos = currentPos + spawnOffset;
+                Debug.Log(i);
+                pos.transform.position = spawnPos;
+            }
+        }
+    }
+
     private void ChoosePoint()
     {
-        int choose = Random.Range(0, collisionPoints.Count - 1);
-        Transform choose2 = collisionPoints[choose].transform;
+        if (collisionPoints.Count > 0)
+        {
+            int choose = Random.Range(0, collisionPoints.Count);
+            Transform choose2 = collisionPoints[choose].transform;
 
-        MoveCowToPoint(choose2);
-
+            MoveCowToPoint(choose2);
+        }
 
     }
 
     private void MoveCowToPoint(Transform destinty)
     {
+        currentCowState = cowStates.Moving;
         Vector3 currentPos = transform.position;
-        currentPos.x =  Mathf.MoveTowards(transform.position.x, destinty.transform.position.x,2.0f);
-        currentPos.z =  Mathf.MoveTowards(transform.position.z, destinty.transform.position.z,2.0f);
-        transform.position = currentPos;
+        this.transform.LookAt(destinty.transform);
+        this.gameObject.transform.DOMove(destinty.position, 1.0f).OnComplete(() =>
+        {
+            MovePointsCow();
+            currentCowState = cowStates.Idle;
+
+        } );
+
+    }
+
+    IEnumerator waitToChoosePoint()
+    {
+       
+            currentCowState = cowStates.Moving;
+            yield return new WaitForSeconds(2);
+            ChoosePoint();
+            Debug.Log("Corruitna");
+        
+     
     }
 }
