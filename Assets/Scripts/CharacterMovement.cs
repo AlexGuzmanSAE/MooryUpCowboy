aee6ec7f29819a6afdb77edbe926db59a95ff3ef
JaimeAyoub <ayoubramosjaime@gmail.com>
2026-06-08 17:48:12 -0600

    weas

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+public class CharacterMovement : MonoBehaviour
+{
+
+    [SerializeField] private InputActionReference leftHand;
+    public Vector3 velocity;
+    
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+           
+        if(leftHand == null)
+        {
+            Debug.Log("NO HAND");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if( leftHand != null)
+        {
+            velocity = leftHand.action.ReadValue<Vector3>();
+            Debug.Log(velocity);
+        }
+        else
+        {
+            Debug.Log("NO");
+        }
+
+       
+    }
+}
